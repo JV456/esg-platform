c06ba3a6f66b5b0cc1d1e61953b45c267eebf114
Jai Vadula <jaivadula@gmail.com>
2026-05-28 23:15:57 +0530

    superuser

---
+++ breathe-esg/backend/create_superuser.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+
+username = "admin"
+
+password = "admin123"
+
+email = "admin@example.com"
+
+
+if not User.objects.filter(username=username).exists():
+
+    User.objects.create_superuser(
+        username=username,
+        password=password,
+        email=email
+    )
+
+    print("Superuser created.")
+
+else:
+
+    print("Superuser already exists.")
